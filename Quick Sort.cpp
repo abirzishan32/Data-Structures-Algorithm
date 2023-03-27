69fb0ad58d27141c0249b5fa7b711dd72e03f344
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2023-03-27 20:56:12 +0600

    Create Quick Sort.cpp

---
+++ Quick Sort.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define pb push_back
+#define N 10000005
+#define div 1000003
+#define mod 1000000007
+#define MOD 998244353
+#define fr(i,a,b) for(int i = a;i<b;i++)
+#define fri(i,a,b) for(int i = a;i>=b;i--)
+#define pyn(flag) cout<<(flag?"YES":"NO")<<endl;
+#define vrev reverse(v.begin(), v.end())
+#define vin cin>>v[i]
+#define vsort sort(v.begin(), v.end())
+#define yout cout<<"YES"<<endl
+#define nout cout<<"NO"<<endl;
+
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void partition(int arr[], int low, int high, int& pivot_index) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i+1], arr[high]);
+    pivot_index = i + 1;
+}
+
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot_index;
+        partition(arr, low, high, pivot_index);
+        quicksort(arr, low, pivot_index - 1);
+        quicksort(arr, pivot_index + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 0, 6, -1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quicksort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
