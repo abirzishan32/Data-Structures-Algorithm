b72ff87abb8fe67adaadd14244f08af02333848c
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2023-03-29 04:34:06 +0600

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,20 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-void swp(int *a , int *b){
-    *a ^= *b ^= *a ^= *b ;
+
+
+void Swap(int &a , int &b){
+    a = a+b;
+    b = a-b;
+    a = a-b;
 }
-void bubbleSort(int *a , int n){
+void bubbleSort(int *ara , int n){
     for(int i = 0 ; i < n-1 ; i++){
+        bool flag=false;
         for(int j = 0 ; j < n - i - 1 ; j++){
-            if(a[j] > a[j+1]) swp(&a[j] , &a[j+1]) ;
+            if(ara[j] > ara[j+1]) {
+                Swap(ara[j] , ara[j+1]);
+                flag=true;
+            }
+        }
+        if(!flag){
+            break;
         }
     }
 }
+
 int main(){
-    int n ;
-    cin >> n ;
-    int a[n] ;
-    for(int i = 0 ; i < n ; i++) cin >> a[i] ;
-    bubbleSort(a , n) ;
-    for(int i = 0 ; i < n ; i++) cout << a[i] << " " ;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n ;
+  cin >> n ;
+  int a[n] ;
+  for(int i = 0 ; i < n ; i++) cin >> a[i] ;
+  bubbleSort(a , n) ;
+  for(int i = 0 ; i < n ; i++) cout << a[i] << " " ;
+  return 0;
 }
