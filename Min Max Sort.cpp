2e25ea4fb96f966c360713bf4f3c0c133d8ce5f1
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2023-04-02 03:06:10 +0600

    Update Min Max Sort.cpp

--- Min Max Sort.cpp
+++ Min Max Sort.cpp
@@ -1,38 +1,73 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define div 1000003
+#define mod 1000000007
+#define MOD 998244353
+#define N 10000005
+#define fr(i,a,b) for(int i = a;i<b;i++)
+#define fri(i,a,b) for(int i = a;i>=b;i--)
+#define pb push_back
+#define pyn(flag) cout<<(flag?"YES":"NO")<<endl;
+#define vin cin>>v[i]
+#define vsort sort(v.begin(), v.end())
+#define vrev reverse(v.begin(), v.end())
+#define yout cout<<"YES"<<endl
+#define nout cout<<"NO"<<endl
+
 void swp(int &a , int &b){
     a ^= b ^= a ^= b ;
 }
-void selectMinMax(int arr[], int n, int &minIndex, int &maxIndex) {
-    minIndex = maxIndex = 0;
-    for (int i = 1; i < n; i++) {
-        if (arr[i] < arr[minIndex]) {
+
+
+void findMinMax (int *ara, int n, int start, int end, int &minIndex, int &maxIndex){
+    minIndex = start;
+    maxIndex = start; 
+    for(int i=start; i<=end; i++){
+        if(ara[i] < ara[minIndex]){
             minIndex = i;
         }
-        if (arr[i] > arr[maxIndex]) {
+        else if(ara[i] > ara[maxIndex]){
             maxIndex = i;
         }
     }
 }
 
-void minMaxSelectionSort(int arr[], int n) {
-    for (int i = 0; i < n/2; i++) {
+void minMaxSort(int *ara, int n)
+{
+    for(int i=0, j=n-1; i<j; i++, j--){
         int minIndex, maxIndex;
-        selectMinMax(arr + i, n - 2*i, minIndex, maxIndex);
-        minIndex += i;
-        maxIndex += i;
-        swap(arr[i], arr[minIndex]);
-        if (maxIndex == i) {
+        findMinMax(ara, n, i, j, minIndex, maxIndex);
+        if(minIndex != i){
+            swp(ara[i], ara[minIndex]);
+        }
+        if(maxIndex == i){
             maxIndex = minIndex;
         }
-        swap(arr[n - 1 - i], arr[maxIndex]);
+        if(maxIndex != j){
+            swp(ara[j], ara[maxIndex]);
+        }
     }
 }
+
 int main(){
-    int n ;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n ;
     cin >> n ;
     int a[n] ;
     for(int i = 0 ; i < n ; i++) cin >> a[i] ;
-    minMaxSelectionSort(a , n) ;
+    minMaxSort(a, n);
     for(int i = 0 ; i < n ; i++) cout << a[i] << " " ;
+  return 0;
 }
