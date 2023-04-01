b4a99ef6207620cc4347d6656b6431ef1f794bd4
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2023-04-02 04:41:19 +0600

    Update Quick Sort.cpp

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -25,28 +25,31 @@ typedef vector<vl> vvl;
 #define nout cout<<"NO"<<endl;
 
 
-void swap(int& a, int& b) {
+void Swap(int& a, int& b) {
     int temp = a;
     a = b;
     b = temp;
 }
 
-void partition(int arr[], int low, int high, int& pivot_index) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+void partition(int *arr, int low, int high, int& pivot_index) {
+    int pivot = arr[low];
+    int start = low, end = high;
+    while(start < end){
+        while(arr[start] <= pivot){
+            start++;
+        }
+        while (arr[end] > pivot){
+            end--;
+        }
+        if(start < end){
+            Swap(arr[start], arr[end]);
         }
     }
-
-    swap(arr[i+1], arr[high]);
-    pivot_index = i + 1;
+    Swap(arr[low], arr[end]);
+    pivot_index = end;
 }
 
-void quicksort(int arr[], int low, int high) {
+void quicksort(int *arr, int low, int high) {
     if (low < high) {
         int pivot_index;
         partition(arr, low, high, pivot_index);
