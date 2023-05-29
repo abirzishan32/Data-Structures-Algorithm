e8d203ecf525733acbe000ed5d913cd0335136e7
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2023-05-30 02:45:24 +0600

    Create Topological Sort.cpp

---
+++ Topological Sort.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define div 1000003
+#define mod 1000000007
+#define MOD 998244353
+#define N 10000005
+#define fr(i,a,b) for(int i = a;i<b;i++)
+#define fri(i,a,b) for(int i = a;i>=b;i--)
+#define pb push_back
+#define pyn(flag) cout<<(flag?"YES":"NO")<<endl;
+#define vin cin>>v[i]
+#define vsort sort(v.begin(), v.end())
+#define vrev reverse(v.begin(), v.end())
+#define yout cout<<"YES"<<endl
+#define nout cout<<"NO"<<endl
+
+void dfs(int node, vector<vector<int> >& adj, vector<bool>& vis, stack<int>& st) {
+    vis[node] = true;
+    for (auto it : adj[node]) {
+        if (!vis[it]) {
+            dfs(it, adj, vis, st);
+        }
+    }
+    st.push(node);
+}
+
+vector<int> TopoSort(int n, vector<vector<int> >& adj) {
+    vector<bool> vis(n, false);
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) {
+            dfs(i, adj, vis, st);
+        }
+    }
+    vector<int> ans;
+    while (!st.empty()) {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int V, E;
+    cin >> V >> E;
+    vector<vector<int> > adj(V);
+
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+    vector<int> topo = TopoSort(V, adj);
+
+    for (int node : topo) {
+        cout << node << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
